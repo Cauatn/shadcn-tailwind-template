9912cd27ccdc620d71523051e28046a27904ad71
Cauã Tavares <tavarescauac@gmail.com>
2024-02-08 11:41:38 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/ui/button'
+
+function App() {
+    return (
+        <>
+            <div className="flex h-screen w-screen items-center justify-center">
+                <Button>Hello World!</Button>
+            </div>
+        </>
+    )
+}
+
+export default App
